4269dd24bebed837f316543e46591574fb8ae40c
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2025-07-21 21:11:17 +0100

    Adding Code to take into account project path as a paramter

--- generate_makefile_from_bash.sh
+++ generate_makefile_from_bash.sh
@@ -1,3 +1,21 @@
 #!/bin/bash 
 
+PROJECT_PATH=""  #global variable to store the directory path
+
+
+while [[ $# -gt 0 ]] ; do
+  case "$1" in 
+   --path)
+   PROJECT_PATH="$2"
+   shift 2  ;;
+  
+   *) 
+   echo "Error" ;;
+  esac
+done 
+# to check wether a path was passed or not
+if [ -z $PROJECT_PATH ] ; then 
+ echo "provide a path please"
+ exit 1 
+fi
 
