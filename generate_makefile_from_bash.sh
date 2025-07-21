7aee5c37b5c64bd0691ae7760a7734d65ad96103
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2025-07-21 21:25:50 +0100

    Adding option to enable c or c++ code compiling

--- generate_makefile_from_bash.sh
+++ generate_makefile_from_bash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
-PROJECT_PATH=""  #global variable to store the directory path
-
+#global variable to store the directory path
+PROJECT_PATH=""  
+ COMPILE_FILE=""
 
 while [[ $# -gt 0 ]] ; do
   case "$1" in 
@@ -9,13 +10,29 @@ while [[ $# -gt 0 ]] ; do
    PROJECT_PATH="$2"
    shift 2  ;;
   
+   --compile_file)
+   COMPILE_FILE="$2"
+   shift 2 ;;
+   
    *) 
-   echo "Error" ;;
+   echo "Error" 
+   shift 2 
+   exit 1 ;;
   esac
 done 
 # to check wether a path was passed or not
-if [ -z $PROJECT_PATH ] ; then 
+if [ -z $PROJECT_PATH ] ; then  # the -z option means that the length of string is equal to zero
  echo "provide a path please"
  exit 1 
 fi
+
+if [ $COMPILE_FILE == "c" ] ; then 
+ CC=gcc 
+ 
+ elif [ $COMPILE_FILE == "c++" ] ; then 
+  CC=g++ 
+  else 
+      echo "error in the source code to compile "
+      fi 
+      
 
